013b2255472e82e15b07ad381b652ba4c82395ba
sultan yaşar <129904909+atlasayaz12@users.noreply.github.com>
2024-01-12 14:55:29 +0300

    index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>yapay zeka işte </title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
+    
 <body>
     <style>
         body{
