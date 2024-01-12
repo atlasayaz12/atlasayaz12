5d44ac397684fb0c99d5c2de63bae2a3d2f50d90
sultan yaşar <129904909+atlasayaz12@users.noreply.github.com>
2024-01-12 14:48:32 +0300

    Add files via upload

---
+++ chat-api.php
@@ -0,0 +1,45 @@
+<?php
+ 
+
+/*
+ * 
+ *  BU PROJEDE HİÇ BİR GÜVENLİK ÖNLEMİ ALINMAMIŞTIR
+ * 
+*/
+if(@$_POST){
+
+    $api_key = "sk-mqCV1jKskIAAr592reS8T3BlbkFJ5V1Y8c@xaYdun3y0VsaQ";
+    $prompt = $_POST['prompt'];
+    
+    $stop = array(" Human:", " AI:");
+    $data = array(
+        "model" => "text-davinci-003",
+        "prompt" => $prompt,
+        "temperature" => 0.9,
+        "max_tokens" => 500,
+        "top_p" => 1,
+        "frequency_penalty" => 0,
+        "presence_penalty" => 0.6,
+        "stop" => $stop
+    );
+
+    
+    $payload = json_encode($data);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://chat.openai.com/');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json', 
+        'Authorization: Bearer ' . $api_key
+    ));
+    $resault = curl_exec($ch);
+    curl_close($ch);
+
+    $response = json_encode($resault, true);
+    print_r($response);
+
+}
+
+?>
